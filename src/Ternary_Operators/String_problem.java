1d530aeda100be1739531d3e47091b15641a533c
dheerajkumar2499 <dheerajkumar.2499@gmail.com>
2025-09-23 22:34:13 +0530

    + operator nature for strings

---
+++ src/Ternary_Operators/String_problem.java
@@ -0,0 +1,12 @@
+package Ternary_Operators;
+
+public class String_problem {
+    public static void main(String[] args) {
+        int a=45;
+        int b=90;
+        String First_name= "Dheeraj";
+                String last_name= "Kumar";
+        System.out.println(First_name+last_name+a+b);
+        System.out.println(a+b+ First_name+ last_name);
+    }
+}
