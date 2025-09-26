bb16f25883c85ad68bbf3d71cf9f750a0e332dbd
dheerajkumar2499 <dheerajkumar.2499@gmail.com>
2025-09-26 23:04:07 +0530

    CLi command line

---
+++ src/CLI_Commandline/Cli.java
@@ -0,0 +1,15 @@
+package CLI_Commandline;
+
+public class Cli {
+    public static void main(String[] args)
+    {
+        String age= args[0];
+        String age_1= args[1];
+        String age_2= args[2];
+        String age_3= args[3];
+        System.out.println(age);
+        System.out.println(age_1);
+        System.out.println(age_2);
+        System.out.println(age_3);
+    }
+}
