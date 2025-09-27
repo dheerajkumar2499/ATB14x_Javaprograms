80e4c13a12d6fee28e2061f7bf3a67890f182462
dheerajkumar2499 <dheerajkumar.2499@gmail.com>
2025-09-27 22:09:54 +0530

    Typecasting

---
+++ src/Type_Casting/Casting.java
@@ -0,0 +1,16 @@
+package Type_Casting;
+
+public class Casting {
+    public static void main(String[] args) {
+        byte b= 1;
+        short sh=34;
+        char ch='A';
+        float f=234f;
+        double d=345;
+        boolean bo= true;
+        int i=55;
+        byte b2=(byte)sh;
+        System.out.println(sh);
+
+    }
+}
---
+++ src/Type_Casting/Casting2.java
@@ -0,0 +1,10 @@
+package Type_Casting;
+
+public class Casting2 {
+    public static void main(String[] args) {
+        int fees=2300;
+        float tax= 3.18F;
+        float fee= (float) fees;
+        System.out.println(fee+tax);
+    }
+}
