a30258ea511ab3821997988706cda4ee75c74f11
dheerajkumar2499 <dheerajkumar.2499@gmail.com>
2025-10-15 21:51:03 +0530

    Escape sequence

---
+++ src/ex_03_Java_Basics/EscapeSequence.java
@@ -0,0 +1,22 @@
+package ex_03_Java_Basics;
+
+public class EscapeSequence {
+    public static void main(String[] args) {
+       // char new_line='\n';
+        char tab_line='\t';
+        char back_space='\b';
+        char carriage_return='\r';
+        //System.out.println("Dheeraj kumar");
+       // System.out.println("dheeraj"+new_line+"kumar");
+       // System.out.println("dheeraj"+tab_line+"kumar");
+       System.out.println("dheeraj"+back_space+"kumar");
+       // System.out.println("dheeraj"+carriage_return+"kumar");
+        System.out.println("Dheeraj\nkumar");
+
+
+    }
+
+    {
+
+    }
+}
