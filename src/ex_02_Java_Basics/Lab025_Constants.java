e58d0534423b53f31086cb0bff3445529be0671c
dheerajkumar2499 <dheerajkumar.2499@gmail.com>
2025-10-21 22:10:40 +0530

    basics

---
+++ src/ex_02_Java_Basics/Lab025_Constants.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab025_Constants {
+    public static void main(String[] args) {
+        int a=34;
+         a = 66;
+         final int b = 56;
+
+        System.out.println(b);
+    }
+}
